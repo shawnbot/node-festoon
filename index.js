ac618b1e72a64832f4267e5f20ec3a64b86bd046
Shawn Allen <shawn.allen@gsa.gov>
2015-06-05 16:36:19 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,282 @@
+var extend = require('extend');
+var async = require('async');
+var fs = require('fs');
+var joinPath = require('path').join;
+var tito = require('tito');
+
+var Counselor = function(config) {
+  // support calling Counselor() without `new`
+  if (!(this instanceof Counselor)) {
+    return new Counselor(config);
+  }
+
+  this.config = config || {};
+  this.sources = {};
+  this.loaders = extend({}, Counselor.loaders, this.config.loaders);
+
+  this.interpolationPattern = this.config.interpolationPattern || /:(\w+)/g;
+
+  if (this.config.sources) {
+    this.addSources(this.config.sources);
+  }
+};
+
+Counselor.prototype = {
+
+  addSources: function(sources) {
+    if (Array.isArray(sources)) {
+      sources.forEach(function(source) {
+        this.setSource(source.id, source);
+      }, this);
+    } else {
+      for (var id in sources) {
+        this.setSource(id, sources[id]);
+      }
+    }
+    return this;
+  },
+
+  setSource: function(id, source) {
+    if (!id) throw new Error('invalid source id: "' + id + '"');
+    this.sources[id] = source;
+    return this;
+  },
+
+  setSources: function(sources) {
+    if (!sources || typeof sources !== 'object') {
+      throw new Error('sources must be an Object, got ' + typeof sources);
+    }
+    this.sources = sources;
+    return this;
+  },
+
+  getLoader: function(sources) {
+    var self = this;
+    return function loader(req, res, next) {
+      if (!res.data) res.data = {};
+      var params = extend({}, req.params, req.query);
+      self.load(sources, params, function(error, data) {
+        if (error) return next(error);
+        extend(res.data, data);
+        return next();
+      });
+    };
+  },
+
+  load: function(sources, params, next) {
+    if (arguments.length === 2) {
+      next = params;
+      params = {};
+    }
+    sources = this.interpolateSources(sources, params);
+    return this.loadSources(sources, params, next);
+  },
+
+  interpolateSources: function(sources, params) {
+    if (sources === '*') {
+      sources = objectify(Object.keys(this.sources));
+    } else if (typeof sources === 'string') {
+      sources = objectify([sources]);
+    } else if (typeof sources === 'object') {
+      if (Array.isArray(sources)) {
+        sources = objectify(sources);
+      }
+    }
+
+    for (var dest in sources) {
+      var src = sources[dest];
+      if (!src) throw new Error('no such data source: "' + src + '"');
+      sources[dest] = this.interpolate(this.sources[src], params);
+    }
+
+    return sources;
+  },
+
+  interpolate: function(value, params) {
+    if (typeof value === 'object') {
+      return mapObject(value, this.interpolate, this);
+    } else if (typeof value === 'function') {
+      return value;
+    }
+
+    // always coerce to a string
+    value = String(value).replace(this.interpolationPattern, function(_, key) {
+      if (!(key in params)) {
+        throw new Error('non-existent key: "' + key + '" in "' + value + '"');
+      }
+      return params[key];
+    });
+
+    // follow references to other data sources
+    if (value.charAt(0) === '#') {
+      var id = value.substr(1);
+      if (!(id in this.sources)) {
+        throw new Error('bad source reference: "' + value + '"');
+      }
+      value = this.interpolate(this.sources[id]);
+    }
+
+    return value;
+  },
+
+  loadSources: function(sources, params, done) {
+    var data = sources[0] ? [] : {};
+    var self = this;
+    async.map(Object.keys(sources), function(id, next) {
+      self.loadSource(sources[id], params, function(error, source) {
+        data[id] = source;
+        return next(error);
+      });
+    }, function(error) {
+      done(error, data);
+    });
+  },
+
+  loadSource: function(source, params, done) {
+    switch (typeof source) {
+      case 'string':
+        return this.loadFile(source, done);
+
+      case 'function':
+        return source.call(this, params, done);
+
+      case 'object':
+        if (source.file) {
+          return this.loadFile(source.file, done);
+        } else if (Array.isArray(source)) {
+          var self = this;
+          return async.map(source, function(src, next) {
+            self.loadSource(src, params, next);
+          }, done);
+        } else {
+          return this.loadSources(source, params, done);
+        }
+    }
+    throw new Error('invalid source type: ' + typeof source);
+  },
+
+  loadFile: function(filename, done) {
+    var path = this.config.path;
+    if (path) filename = joinPath(path, filename);
+    var ext = filename.split('.').pop();
+    if (ext in this.loaders) {
+      return this.loaders[ext].call(this, filename, done);
+    } else {
+      var loader = this.loaders['default'];
+      if (loader) return loader.call(this, filename, done);
+    }
+    throw new Error('no loader found for: "' + filename + '"');
+  }
+
+};
+
+Counselor.getTabularLoader = function(type, options) {
+  return function(filename, done) {
+    var parse = tito.formats.createReadStream(type, options);
+    var data = [];
+    return fs.createReadStream(filename)
+      .on('error', done)
+      .pipe(parse)
+      .on('error', done)
+      .on('data', function(d) {
+        data.push(d);
+      })
+      .on('end', function() {
+        done(null, data);
+      });
+  };
+};
+
+Counselor.loadJSON = function(filename, done) {
+  fs.readFile(filename, function(error, buffer) {
+    if (error) return done(error);
+    try {
+      var data = JSON.parse(buffer.toString());
+    } catch (error) {
+      return done(error);
+    }
+    return done(null, data);
+  });
+};
+
+Counselor.loadText = function(filename, done) {
+  fs.readFile(filename, function(error, buffer) {
+    if (error) return done(error);
+    return done(null, buffer.toString());
+  });
+};
+
+Counselor.loaders = {
+  json: Counselor.loadJSON,
+  csv: Counselor.getTabularLoader('csv'),
+  tsv: Counselor.getTabularLoader('tsv'),
+  txt: Counselor.loadText,
+  // 'default': Counselor.loadText
+};
+
+Counselor.transform = function(source, transform) {
+  return function(params, done) {
+    this.load(source, params, function(error, data) {
+      if (source in data) {
+        data = data[source];
+      } else {
+        console.warn('data lacks key:', source);
+      }
+      return error
+        ? done(error)
+        : done(null, transform(data, params));
+    });
+  };
+};
+
+// creates a lookup function that returns the first value for which
+// the test() function returns truthy in the data source with the
+// given id.
+Counselor.findByParam = function(sourceId, param, key) {
+  if (typeof sourceId !== 'string') {
+    throw new Error('findByParam() requires a single source id as the first argument');
+  }
+  if (!key) key = param;
+  return Counselor.transform(sourceId, function(data, params) {
+    var id = params[param];
+    return data.filter(function(d, i) {
+      return d[key] == id;
+    })[0];
+  });
+};
+
+Counselor.transform.filter = function(sourceId, filter) {
+  if (typeof sourceId !== 'string') {
+    throw new Error('findByParam() requires a single source id as the first argument');
+  }
+  return Counselor.transform(sourceId, function(data, params) {
+    return Array.isArray(data)
+      ? data.filter(filter, params)
+      : filter.call(params, data);
+  });
+};
+
+function objectify(keys, values) {
+  var obj = {};
+  keys.forEach(function(key) {
+    obj[key] = values ? values[key] : key;
+  });
+  return obj;
+}
+
+function mapObject(obj, fn, thisArg) {
+  var out = {};
+  for (var key in obj) {
+    out[key] = fn.call(thisArg, obj[key], key);
+  }
+  return out;
+}
+
+function getFilenameRelativeTo(filename, path) {
+  if (!path) return filename;
+  else if (filename.match(/^[.\/]/)) return filename;
+  else if (path.match(/\/$/)) return path + filename;
+  return [path, filename].join('/');
+}
+
+module.exports = Counselor;
---
+++ test/spec.js
@@ -0,0 +1,293 @@
+var Counselor = require('../');
+var assert = require('assert');
+
+var FIXTURES = {
+  path: __dirname + '/fixtures',
+  sources: {
+    foo: 'foo.csv',
+    list: ['foo.csv', 'bar.csv'],
+    named: {
+      foo: 'foo.csv',
+      bar: 'bar.csv'
+    }
+  }
+};
+
+describe('Counselor()', function() {
+
+  it('returns a Counselor instance without `new`', function() {
+    var c = Counselor();
+    assert.ok(c instanceof Counselor);
+  });
+
+  it('initializes data sources', function() {
+    var c = new Counselor({
+      sources: {
+        foo: 'bar.csv'
+      }
+    });
+    assert.ok(c.sources.foo, 'no source "foo" provided in config');
+  });
+
+  describe('setSource()', function() {
+    it('sets a named source', function() {
+      var c = new Counselor(FIXTURES);
+      c.setSource('foo', ':foo.csv');
+      assert.equal(c.sources.foo, ':foo.csv');
+    });
+  });
+
+  describe('setSources()', function() {
+
+    it('sets all sources', function() {
+      var c = new Counselor(FIXTURES);
+      c.setSources({x: 'foo.csv'});
+      assert.ok(!c.sources.foo);
+      assert.ok(c.sources.x);
+    });
+
+    it('throws an error on non-Objects', function() {
+      var c = new Counselor();
+      assert.throws(function() { c.setSources('foo'); });
+      assert.throws(function() { c.setSources(null); });
+      assert.throws(function() { c.setSources(123); });
+    });
+
+  });
+
+  describe('load()', function() {
+
+    beforeEach(createWithFixtures);
+    afterEach(removeCounselor);
+
+    it('loads a single source as a string', function(done) {
+      this.counselor.load('foo', function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.foo, 'object', 'no "foo" data');
+        done();
+      });
+    });
+
+    it('throws an error for invalid sources', function() {
+      assert.throws(function() {
+        this.load('0xBADCAFE', function() { });
+      }.bind(this));
+    });
+
+    it('loads a single source as an Array', function(done) {
+      this.counselor.load(['foo'], function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.foo, 'object', 'no "foo" data');
+        done();
+      });
+    });
+
+    it('loads an aliased data source', function(done) {
+      this.counselor.load({bar: 'foo'}, function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.bar, 'object', 'no "bar" data: ' + Object.keys(data));
+        done();
+      });
+    });
+
+    it('loads Array data sources', function(done) {
+      this.counselor.load('list', function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.list, 'object');
+        assert.ok(Array.isArray(data.list), 'data is not an Array');
+        done();
+      });
+    });
+
+    it('loads named (map) data sources', function(done) {
+      this.counselor.load('named', function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.named, 'object');
+        assert.ok(Array.isArray(data.named.foo), 'data is not an Array');
+        done();
+      });
+    });
+
+    it('loads data source references', function(done) {
+      this.counselor.setSource('ref', '#foo');
+      this.counselor.load(['ref', 'foo'], function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.ref, 'object');
+        assert.equal(typeof data.foo, 'object');
+        assert.deepEqual(data.ref, data.foo);
+        done();
+      });
+    });
+
+    it('loads interpolated references', function(done) {
+      this.counselor.setSource('ref', '#:bar');
+      this.counselor.load(['ref', 'foo'], {bar: 'foo'}, function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.ref, 'object');
+        assert.equal(typeof data.foo, 'object');
+        assert.deepEqual(data.ref, data.foo);
+        done();
+      });
+    });
+
+    it('loads function data sources', function(done) {
+      this.counselor.setSource('func', function(params, done) {
+        return done(null, params);
+      });
+      var params = {x: 1, y: 2};
+      this.counselor.load('func', params, function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.strictEqual(data.func, params);
+        done();
+      });
+    });
+
+    it('loads all data sources with "*"', function(done) {
+      this.counselor.setSources({x: 'foo.csv', y: 'bar.csv'});
+      this.counselor.load('*', function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.deepEqual(Object.keys(data), ['x', 'y']);
+        done();
+      });
+    });
+
+  });
+
+  describe('getLoader()', function() {
+
+    beforeEach(createWithFixtures);
+    afterEach(removeCounselor);
+
+    it('returns an Express-compatible function: `load(req, res, next)`', function() {
+      var load = this.counselor.getLoader('foo');
+      assert.equal(load.length, 3);
+    });
+
+    it('loads data from a request', function(done) {
+      var load = this.counselor.getLoader('foo');
+      var req = {};
+      var res = {};
+      load(req, res, function(error) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof res.data, 'object');
+        assert.equal(typeof res.data.foo, 'object');
+        done();
+      });
+    });
+
+    it('interpolates data sources', function(done) {
+      this.counselor.setSource('bar', ':bar.csv');
+      this.counselor.load('bar', {bar: 'foo'}, function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.bar, 'object');
+        done();
+      });
+    });
+
+    it('throws errors for missing interpolation placeholders', function() {
+      this.counselor.setSource('bar', ':bar.csv');
+      assert.throws(function() {
+        this.counselor.load('bar', {baz: 'foo'}, function() { });
+      }.bind(this));
+    });
+
+  });
+
+  describe('Counselor.transform()', function() {
+
+    beforeEach(createWithFixtures);
+    afterEach(removeCounselor);
+
+    it('transforms data', function(done) {
+      this.counselor.setSource('length', Counselor.transform('foo', function(data) {
+        return data.length;
+      }));
+
+      this.counselor.load('length', function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.strictEqual(data.length, 2);
+        done();
+      });
+    });
+
+  });
+
+  describe('Counselor.transform.filter()', function() {
+
+    beforeEach(createWithFixtures);
+    afterEach(removeCounselor);
+
+    it('filters arrays', function(done) {
+      this.counselor.setSource('filter', Counselor.transform.filter('foo', function(d) {
+        return d.a == 1;
+      }));
+      this.counselor.load('filter', function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.ok(Array.isArray(data.filter), 'data.filter is not an Array');
+        assert.equal(data.filter.length, 1, 'filtered data length != 1');
+        done();
+      });
+    });
+
+    it('filters arrays by parameter value', function(done) {
+      this.counselor.setSource('filter', Counselor.transform.filter('foo', function(d) {
+        return d.a == this.a;
+      }));
+      this.counselor.load('filter', {a: 1}, function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.ok(Array.isArray(data.filter), 'data.filter is not an Array');
+        assert.equal(data.filter.length, 1, 'filtered data length != 1');
+        done();
+      });
+    });
+
+    it('recognizes objects uses `filter.call(params, data)`', function(done) {
+      this.counselor.setSource('filter', Counselor.transform.filter('named', function(data) {
+        return data.foo;
+      }));
+      this.counselor.load('filter', function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.ok(Array.isArray(data.filter), 'data.filter is not an Array');
+        assert.equal(data.filter.length, 2, 'filtered data length != 2');
+        done();
+      });
+    });
+
+  });
+
+  describe('Counselor.transform.findByParam()', function() {
+
+    beforeEach(createWithFixtures);
+    afterEach(removeCounselor);
+
+    it('does lookups by key', function(done) {
+      this.counselor.setSource('lookup', Counselor.findByParam('foo', 'a'));
+      var params = {a: 1};
+      this.counselor.load('lookup', params, function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.lookup, 'object');
+        done();
+      });
+    });
+
+    it('does lookups from one key to another', function(done) {
+      this.counselor.setSource('lookup', Counselor.findByParam('foo', 'b', 'a'));
+      var params = {b: 1};
+      this.counselor.load('lookup', params, function(error, data) {
+        assert.ok(!error, 'error: ' + error);
+        assert.equal(typeof data.lookup, 'object');
+        done();
+      });
+    });
+
+  });
+
+});
+
+function createWithFixtures() {
+  this.counselor = new Counselor(FIXTURES);
+}
+
+function removeCounselor() {
+  this.counselor = null;
+}
